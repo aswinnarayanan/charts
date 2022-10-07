a8a947c78d5c893488c953224bb0e3e1bf8b487c
aswinnarayanan <aswinn89@gmail.com>
2022-10-07 07:39:01 +0000

    Add initialise and build scripts

---
+++ 1-init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo snap install microk8s --classic
+microk8s enable community
+microk8s enable dns fluentd ingress metrics-server prometheus rbac registry storage
+
+helm repo add ais https://australian-imaging-service.github.io/charts
+helm repo update
+
+kubectl delete namespaces ctp
+kubectl create namespace ctp
+
+# helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+# helm repo update
+# helm install ingress-nginx ingress-nginx/ingress-nginx
---
+++ 2-update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+helm upgrade ctp ./releases/ctp --install --values ./my-ctp-site-overrides.yaml --namespace ctp
+
+# pub_ip=$(microk8s kubectl get node --output jsonpath='{.items[0].status.addresses[?(@.type == "InternalIP")].address}')
+# microk8s kubectl -n ctp patch svc ctp-httpsimport -p '{"spec":{"externalIPs":["'${pub_ip}'"]}}'
+
+# helm repo add jetstack https://charts.jetstack.io
+# helm repo update
+# helm install \
+#   cert-manager jetstack/cert-manager \
+#   --namespace cert-manager \
+#   --create-namespace \
+#   --version v1.9.1 \
+#   --set installCRDs=true
+
+# kubectl create -n ctp -f issuer-prod.yaml
+# kubectl create -n ctp -f issuer-staging.yaml
+# kubectl create -n ctp -f ingress-tls.yaml
+
+
